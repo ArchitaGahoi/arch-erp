67e63581e4c29e3ce88d1c9c3e4cca72784d58b9
ArchitaGahoi <148310308+ArchitaGahoi@users.noreply.github.com>
2025-08-12 13:04:06 +0530

    Updated components

--- arch-erp-frontend/src/login/page.tsx
+++ arch-erp-frontend/src/login/page.tsx
@@ -1,16 +1,10 @@
 import { LoginForm } from "@/components/login-comp/login-form"
-import { ThemeToggle } from "@/components/login-comp/theme-toggle"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 transition-colors">
-          <div className="absolute top-4 right-4">
-            <ThemeToggle />
-          </div>
-          <LoginForm />
-        </div>
+        <LoginForm />
       </div>
     </div>
   )
