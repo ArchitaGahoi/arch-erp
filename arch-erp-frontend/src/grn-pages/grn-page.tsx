bc2ef33d5004d6c3eb893e0c9a654f9d973d0d0a
ArchitaGahoi <148310308+ArchitaGahoi@users.noreply.github.com>
2025-08-29 08:59:22 +0530

    Updated components

--- arch-erp-frontend/src/grn-pages/grn-page.tsx
+++ arch-erp-frontend/src/grn-pages/grn-page.tsx
@@ -20,6 +20,15 @@ import { Button } from "@/components/ui/button";
 //   unit: string; 
 // };
 
+type ItemDetail = {
+  poitemDetailId: number;
+  itemName: string;
+  poQuantity: number;
+  preRecivedQuantity: number;
+  recivedQuantity: number;
+  balance: number;
+  selected?: boolean;
+};
 
 // type ItemMaster = {
 //   itemId: string;
